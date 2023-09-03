de45191a3e67f2e998b668ec9b635f4a70cf20dc
sarahrdickerson <sarahrdickerson@gmail.com>
2023-09-03 14:53:18 -0500

    problem 3

--- labs/lab1/problem3.py
+++ labs/lab1/problem3.py
@@ -7,7 +7,7 @@ samples = np.random.normal(0, 5, 25000)
 # Then estimate the mean and standard deviation of this gaussian using elementary numpy commands, i.e., addition,
 # multiplication, division (do not use a command that takes data and returns the mean or standard deviation)
 mean = np.sum(samples) / len(samples)
-print("Mean: ", mean)
+print("Estimated mean: ", mean)
 
 stdeviation = np.sqrt(np.sum((samples - mean) ** 2) / len(samples))
-print("Standard Deviation: ", stdeviation)
+print("Estimated standard deviation: ", stdeviation)
