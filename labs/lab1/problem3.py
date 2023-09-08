9610d34e6f0f779587707015c714ac2e3734278d
Jackson Johnston <jacksonjohnston@utexas.edu>
2023-09-08 09:08:25 -0500

    Move files into lab 1 folder

--- labs/lab1/problem3.py
+++ labs/lab1/problem3.py
@@ -1,13 +1,28 @@
 import numpy as np
 
-# Estimate the mean and standard deviation from 1 dimensional data:
-# generate 25,000 samples from a Gaussian distribution with mean 0 and standard deviation 5.
-samples = np.random.normal(0, 5, 25000)
 
-# Then estimate the mean and standard deviation of this gaussian using elementary numpy commands, i.e., addition,
-# multiplication, division (do not use a command that takes data and returns the mean or standard deviation)
-mean = np.sum(samples) / len(samples)
-print("Estimated mean: ", mean)
+def problem3():
+    gauss = np.random.normal(0, 5, 25000)
 
-stdeviation = np.sqrt(np.sum((samples - mean) ** 2) / len(samples))
-print("Estimated standard deviation: ", stdeviation)
+    # Calculate mean
+    mean_sum = 0
+    for i in range(0, 25000):
+        mean_sum += gauss[i]
+
+    mean_estimate = mean_sum / 25000
+
+    # Calculate standard deviation
+    std_sum = 0
+    for i in range(0, 25000):
+        std_sum += (gauss[i] - mean_estimate) ** 2
+
+    std_estimate = np.sqrt(std_sum / 25000)
+
+    print("The mean estimate is: ", mean_estimate)
+    print("The mean of the sum is: ", np.mean(gauss))
+    print("The standard deviation estimate of the sum is: ", std_estimate)
+    print("The standard deviation of the sum is: ", np.std(gauss))
+
+
+
+problem3()
